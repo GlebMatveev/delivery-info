5f2444017fda82afc055cc1e8c2fe996a50c9838
glebmatveev <glebmatveev@gmail.com>
2022-04-08 11:29:27 +0300

    Gettings elements on page

--- delivery-info.js
+++ delivery-info.js
@@ -1,3 +1,9 @@
 function ccStartScript () {
+    let ccDeliveryInterval = document.getElementsByName('ORDER_PROP_24')[0];
+	let ccDeliveryReplacement = document.getElementsByName('ORDER_PROP_25')[0];
+	let ccDeliveryCalendar = document.getElementsByName('ORDER_PROP_26')[0];
 
+    if (ccDeliveryInterval != undefined && ccDeliveryReplacement != undefined && ccDeliveryCalendar != undefined) {
+        console.log("good");
+    }
 }
