15df0ed89116eabcd46e3f5331db9c79fd575dd5
glebmatveev <glebmatveev@gmail.com>
2022-04-08 13:39:04 +0300

    Added filling delivery time intervals

--- delivery-info.js
+++ delivery-info.js
@@ -53,35 +53,106 @@ function ccStartScript () {
 
 
         // listening for datetime picker field changes
-		ccDeliveryCalendar.addEventListener('change', function () {
+        ccDeliveryCalendar.addEventListener('change', function () {
 
-			// getting the time intervals
-			let ccDeliveryInterval = document.getElementsByName('ORDER_PROP_24')[0];
+            // getting the time intervals
+            let ccDeliveryInterval = document.getElementsByName('ORDER_PROP_24')[0];
 
-			// clearing the intervals
-			ccDeliveryInterval.innerHTML = '';
+            // clearing the intervals
+            ccDeliveryInterval.innerHTML = '';
 
-			// current hour
-			Data = new Date();
-			let ccHour = Data.getHours();
-			let ccCurrentHour = parseInt(ccHour, 10);
-			
-			// getting the selected date and conversion
-			let ccSelectedDate = ccDeliveryCalendar.value;
-			let ccArr = ccSelectedDate.split('.'); 
-			let ccArrReversed = ccArr.reverse();
-			ccSelectedDate = ccArrReversed.join('.');
+            // current hour
+            Data = new Date();
+            let ccHour = Data.getHours();
+            let ccCurrentHour = parseInt(ccHour, 10);
+            
+            // getting the selected date and conversion
+            let ccSelectedDate = ccDeliveryCalendar.value;
+            let ccArr = ccSelectedDate.split('.'); 
+            let ccArrReversed = ccArr.reverse();
+            ccSelectedDate = ccArrReversed.join('.');
 
-			let ccCalendarDate = new Date(ccSelectedDate);
+            let ccCalendarDate = new Date(ccSelectedDate);
 
-			
-			// reset the minutes of the current date and time for correct comparison
-			ccCurrentDate = new Date();
-			ccCurrentDate.setHours(0,0,0,0);
+            
+            // reset the minutes of the current date and time for correct comparison
+            ccCurrentDate = new Date();
+            ccCurrentDate.setHours(0,0,0,0);
 
-            console.log(ccCurrentHour);
-            console.log(ccCalendarDate);
-            console.log(ccCurrentDate);
-		});
+            // past, leave delivery intervals empty
+            if (ccCalendarDate.getTime() < ccCurrentDate.getTime()) {
+                console.log("прошлое, не выдаём временные интервалы доставки");
+            };
+            
+            // the future and not today, filling all time intervals of delivery
+            if (ccCalendarDate.getTime() > ccCurrentDate.getTime()) {
+                console.log("будущее и не сегодня, выдаём все временные интервалы доставки");
+                
+                ccDeliveryInterval.options[0] = new Option("00:00 - 02:00", "TIME8");
+                ccDeliveryInterval.options[1] = new Option("02:00 - 04:00", "TIME9");
+                ccDeliveryInterval.options[2] = new Option("04:00 - 06:00", "TIME10");
+                ccDeliveryInterval.options[3] = new Option("06:00 - 08:00", "TIME11");
+                ccDeliveryInterval.options[4] = new Option("08:00 - 10:00", "TIME12");
+                ccDeliveryInterval.options[5] = new Option("10:00 - 12:00", "TIME1");
+                ccDeliveryInterval.options[6] = new Option("12:00 - 14:00", "TIME2");
+                ccDeliveryInterval.options[7] = new Option("14:00 - 16:00", "TIME3");
+                ccDeliveryInterval.options[8] = new Option("16:00 - 18:00", "TIME4");
+                ccDeliveryInterval.options[9] = new Option("18:00 - 20:00", "TIME5");
+                ccDeliveryInterval.options[10] = new Option("20:00 - 22:00", "TIME6");
+                ccDeliveryInterval.options[11] = new Option("22:00 - 00:00", "TIME7");
+            };
+            
+            // today, need to check for the current time
+            if (ccCalendarDate.getTime() == ccCurrentDate.getTime()) {
+                console.log("сегодняшний день, нужна проверка на текущее время");
+                
+                if ((ccCurrentHour >= 0) && (ccCurrentHour <= 8)) {
+                    ccDeliveryInterval.options[0] = new Option("10:00 - 12:00", "TIME1");
+                    ccDeliveryInterval.options[1] = new Option("12:00 - 14:00", "TIME2");
+                    ccDeliveryInterval.options[2] = new Option("14:00 - 16:00", "TIME3");
+                    ccDeliveryInterval.options[3] = new Option("16:00 - 18:00", "TIME4");
+                    ccDeliveryInterval.options[4] = new Option("18:00 - 20:00", "TIME5");
+                    ccDeliveryInterval.options[5] = new Option("20:00 - 22:00", "TIME6");
+                    ccDeliveryInterval.options[6] = new Option("22:00 - 00:00", "TIME7");
+                }
+                if ((ccCurrentHour >= 9) && (ccCurrentHour <= 10)) {
+                    ccDeliveryInterval.options[0] = new Option("12:00 - 14:00", "TIME2");
+                    ccDeliveryInterval.options[1] = new Option("14:00 - 16:00", "TIME3");
+                    ccDeliveryInterval.options[2] = new Option("16:00 - 18:00", "TIME4");
+                    ccDeliveryInterval.options[3] = new Option("18:00 - 20:00", "TIME5");
+                    ccDeliveryInterval.options[4] = new Option("20:00 - 22:00", "TIME6");
+                    ccDeliveryInterval.options[5] = new Option("22:00 - 00:00", "TIME7");
+                }
+                if ((ccCurrentHour >= 11) && (ccCurrentHour <= 12)) {
+                    ccDeliveryInterval.options[0] = new Option("14:00 - 16:00", "TIME3");
+                    ccDeliveryInterval.options[1] = new Option("16:00 - 18:00", "TIME4");
+                    ccDeliveryInterval.options[2] = new Option("18:00 - 20:00", "TIME5");
+                    ccDeliveryInterval.options[3] = new Option("20:00 - 22:00", "TIME6");
+                    ccDeliveryInterval.options[4] = new Option("22:00 - 00:00", "TIME7");
+                }
+                if ((ccCurrentHour >= 13) && (ccCurrentHour <= 14)) {
+                    ccDeliveryInterval.options[0] = new Option("16:00 - 18:00", "TIME4");
+                    ccDeliveryInterval.options[1] = new Option("18:00 - 20:00", "TIME5");
+                    ccDeliveryInterval.options[2] = new Option("20:00 - 22:00", "TIME6");
+                    ccDeliveryInterval.options[3] = new Option("22:00 - 00:00", "TIME7");
+                }
+                if ((ccCurrentHour >= 15) && (ccCurrentHour <= 16)) {
+                    ccDeliveryInterval.options[0] = new Option("18:00 - 20:00", "TIME5");
+                    ccDeliveryInterval.options[1] = new Option("20:00 - 22:00", "TIME6");
+                    ccDeliveryInterval.options[2] = new Option("22:00 - 00:00", "TIME7");
+                }
+                if ((ccCurrentHour >= 17) && (ccCurrentHour <= 18)) {
+                    ccDeliveryInterval.options[0] = new Option("20:00 - 22:00", "TIME6");
+                    ccDeliveryInterval.options[1] = new Option("22:00 - 00:00", "TIME7");
+                }
+                if ((ccCurrentHour >= 19) && (ccCurrentHour <= 20)) {
+                    ccDeliveryInterval.options[0] = new Option("22:00 - 00:00", "TIME7");
+                }
+                if ((ccCurrentHour >= 21) && (ccCurrentHour <= 23)) {
+                    console.log("пустой список, нужно выбрать следующий день, т.к. доставить уже не успеют");
+                }
+            };
+        });
+        console.log("скрипт отработал");
     }
 }
